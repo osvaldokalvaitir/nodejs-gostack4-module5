23ccea880b81ecb92711affced035b564b1f78cb
Osvaldo Kalvaitir Filho <osvaldokalvaitir@outlook.com>
2019-04-09 22:30:34 -0300

    Limpando base de dados

--- src/config/database.js
+++ src/config/database.js
@@ -11,6 +11,7 @@ module.exports = {
   database: process.env.DB_NAME,
   dialect: process.env.DB_DIALECT || "postgres",
   storage: "./__tests__/database.sqlite",
+  logging: false,
   operatorsAliases: false,
   define: {
     timestamps: true,
